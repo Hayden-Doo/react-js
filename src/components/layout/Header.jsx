c723315eeb765094c99ff4a1b24832dab65cd4a5
Hayden-Doo <heton.doo@gmail.com>
2024-03-31 09:30:40 +0900

    add Prettier config

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,9 +1,9 @@
-function Header(){
-    return(
-        <header>
-            <h1>logo</h1>
-        </header>
-    )
+function Header() {
+  return (
+    <header>
+      <h1>logo</h1>{' '}
+    </header>
+  );
 }
 
 export default Header;
